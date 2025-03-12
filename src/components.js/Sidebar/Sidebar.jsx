23551c27a66878d91e0ecbe70ff81da5c90146d8
tewereus <tewereus12312@gmail.com>
2025-03-12 11:29:54 +0300

    initialized the web app

---
+++ src/components.js/Sidebar/Sidebar.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+const Sidebar = () => {
+  return (
+    <div className="">
+      {/* top */}
+      <div className="">
+        <img src="" alt="menu icon" className="" />
+        <div className="">
+          <img src="" alt="plus icon" className="" />
+          <p className="">New Chat</p>
+        </div>
+      </div>
+      {/* bottom */}
+      <div className=""></div>
+    </div>
+  );
+};
+
+export default Sidebar;
